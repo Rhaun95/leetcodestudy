3fba269b09f247b1fc63b7eea927475d5b59ed9b
tngus5040 <soohyeon.kim@student.uni-luebeck.de>
2023-08-23 10:27:06 +0200

    fixed according to feedbacks 1Â~2

---
+++ study1/week1/Reverse_words_in_String_III.java
@@ -0,0 +1,16 @@
+ class Reverse_words_in_String_III {
+     StringBuffer res = new StringBuffer();
+     StringBuffer sb = new StringBuffer();
+    public String reverseWords(String s) {
+
+        for (String toReverse : s.split(" ")) { // linear time complexity O(n), n is the length of the input String
+            sb.append(toReverse); // constant time complexity O(1)
+            res.append(sb.reverse().append(" ")); // linear time complexity O(m)
+            sb.setLength(0); // constant time complexity O(1)
+        }
+        res.setLength(res.length() - 1); // constant time complexity O(1)
+        return res.toString(); // Space Complexity = O(n)
+    }
+// Time Complexity = O(n*m)
+// Space Complexity = O(n)
+}
---
+++ study1/week1/Third_Maximum_Number.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Third_Maximum_Number {
+    int count = 3;            
+    int tmp = Integer.MIN_VALUE;
+
+    public int thirdMax(int[] nums) { // Space Complexity= O(n), n is the length of the Array
+      Arrays.sort(nums); // time complexity = O(n * log(n))
+      if (nums.length < 3) {
+          return nums[nums.length - 1];
+      } else {
+         for (int i = nums.length - 1; i>=0; i--) {// time complexity = O(n), m is the position, where the third maximum number is set
+             if (tmp != nums[i]) {
+                 count -= 1;
+             }
+              tmp = nums[i];
+            if (count == 0) {
+                return tmp;
+            }
+         }
+       return nums[nums.length - 1];
+      }
+
+    }
+}
+
+// Time Complexity :  O(n * log(n))
+// Space Complexity :  O(1)
