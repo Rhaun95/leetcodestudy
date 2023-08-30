5415d954a8d651ebc5e0555d2a1899895f6eabeb
tngus5040 <soohyeon.kim@student.uni-luebeck.de>
2023-08-30 12:01:57 +0200

    Range sum of BST commented

--- study1/week2/Range_Sum_of_BST.java
+++ study1/week2/Range_Sum_of_BST.java
@@ -7,14 +7,22 @@ class Range_Sum_of_BST {
             sum += root.val;
         }
 
-        if(root.left != null){
+        if (root.left != null) {
             rangeSumBST(root.left, low, high);
         }
 
-        if(root.right != null){
+        if (root.right != null) {
             rangeSumBST(root.right, low, high);
         }
 
         return sum;
     }
+    /**
+     * Time Complexity : O(n)
+       The algorithm visits each node in the binary tree once, resulting in a linear time complexity.
+
+     * Space Compexity : O(1)
+      Since the algorithm uses only one global variable, there is no need for additional memory space.
+      Thus, the space complexity remains constant.
+    */
 }
