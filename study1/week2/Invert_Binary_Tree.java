784bdf83cf5826310c93e6d50e061f7dfc968857
tngus5040 <soohyeon.kim@student.uni-luebeck.de>
2023-08-30 10:53:52 +0200

    Invert_Binary_Tree solved

---
+++ study1/week2/Invert_Binary_Tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Invert_Binary_Tree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+    /**
+     * Space Complexity : O(2^d), d is the depth of the TreeNode
+     In worst case, the binary tree is balanced and has the depth of d
+
+     * Time Complexity : O(n), n is the number of nodes
+     every Node is visited once in the algorithm. It needs linear time.
+     */
+}
