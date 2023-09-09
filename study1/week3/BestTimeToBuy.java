a3365886dc5ef6efdd16cd298cb9708d77760feb
tngus5040 <soohyeon.kim@student.uni-luebeck.de>
2023-09-09 10:35:54 +0200

    best time to buy and sell solved

---
+++ study1/week3/BestTimeToBuy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int max = 0;
+        int small = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+
+            if (small > prices[i]) {
+                small = prices[i];
+            } else {
+                int diff = prices[i] - small;
+                if (diff > max) {
+                    max = diff;
+                }
+            }
+        }
+        return max;
+    }
+    /**
+     * n: number of prices
+
+     T: O(n)
+     comparison of every possible price pair : O(n*(n-1)/2) = O(n^2)
+
+     S: O(1)
+     max: O(1)
+     small: O(1)
+
+     */
+}
