219023014c25bd0d6727fdb8f0cb320b65c4ba2f
tngus5040 <soohyeon.kim@student.uni-luebeck.de>
2023-09-21 15:49:09 +0200

    longest subsequences solved

--- study1/week5/Solution.java
+++ study1/week5/Solution.java
@@ -1,1 +1,36 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int[] res = new int[queries.length];
+        Arrays.sort(nums);
 
+        int sum = 0;
+        int size = 0;
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                sum = sum + nums[j];
+                if (sum <= queries[i]) {
+                    size++;
+                } else {
+                    break;
+                }
+            }
+            res[i] = size;
+            size = 0;
+            sum = 0;
+        }
+
+        return res;
+    }
+
+    /**
+     * n : size of nums
+     * m : size of queries
+     *
+     * T : O(n*m)
+        in the case, sum of all elements in nums is less or equal than all numbers in queries
+
+     * S : O(m)
+     * can create all the numbers in the queries using the numbers in nums
+         res : O(m)
+     */
+}
